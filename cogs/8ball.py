e575cc0b64c09ae46e5f01ca34f728a9e5a4e862
enriktigasna <42349218+enriktigasna@users.noreply.github.com>
2021-11-07 23:10:34 +0100

    Create 8ball.py

---
+++ cogs/8ball.py
@@ -0,0 +1,41 @@
+import discord
+from discord.ext import commands
+import random
+
+class _8ball(commands.Cog):
+    def __init__(self, client):
+        self.client = client
+
+    @commands.Cog.listener()
+    async def on_ready(self):
+        print("8 Ball Cog Ready")
+
+    @commands.command(name='8ball', aliases=['8'])
+    async def eightball(self, ctx, *, question):
+        responses = ['as I see it, yes',
+                     'Yes.',
+                     'Positive',
+                     'From my point of view, yes',
+                     'Convinced.',
+                     'Most Likley.',
+                     'Chances High',
+                     'No.',
+                     'Negative.',
+                     'Not Convinced.',
+                     'Perhaps.',
+                     'Not Sure',
+                     'Maybe',
+                     'I cannot predict now.',
+                     'Im to lazy to predict.',
+                     '*takes off clothes* shit in my ass',
+                     'Eh..'
+                     ]
+        response = random.choice(responses)
+        embed = discord.Embed(title=":8ball: It has spoken")
+        embed.add_field(name='Question: ', value=f'{question}', inline=True)
+        embed.add_field(name='Answer: ', value=f'{response}', inline=True)
+        embed.set_footer(text=".suggest for answer suggestions.")
+        await ctx.reply(embed=embed)
+
+def setup(client):
+    client.add_cog(_8ball(client))
